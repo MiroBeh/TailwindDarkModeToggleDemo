2da799bb6ac2ef250f8290f27e187081b4a861e4
MiroBeh <miro.behninger@gmail.com>
2020-11-27 18:26:45 +0100

    use material ui toggle for demo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import MiroPic from './images/MiroPic.jpg'
-import { DarkModeToggle } from "./Toggle/DarkModeToggle";
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
 
 export default function App() {
+  const [showDarkMode, setShowDarkMode] = useState(false);
+
   return (
-    <div id="myRoot">
+    <div id="myRoot" className={showDarkMode ? "dark" : ""}>
       <div className="flex flex-col dark:bg-gray-900 h-screen" id="background">
-        <DarkModeToggle />
-        <div className="mt-10 mx-auto dark:bg-gray-800 bg-gray-100 rounded-xl shadow p-10" id="card">
-          <img className="rounded-full w-32 mx-auto" src={MiroPic}/>
+        <FormGroup className="mt-5 mx-auto"> 
+          <FormControlLabel
+            control={<Switch checked={showDarkMode} onClick={() => setShowDarkMode(v => !v)} />}
+          />
+        </FormGroup>
+        <div className="mt-5 mx-auto dark:bg-gray-800 bg-gray-100 rounded-xl shadow p-10" id="card">
+          <img className="rounded-full w-32 mx-auto" alt="face of miro" src={MiroPic}/>
           <div className="text-center">
             <h1 className="text-4xl dark:text-green-400 text-green-600">Miro Behninger</h1>
             <h2 className="text-sm dark:text-gray-300 text-gray-500">Software Engineer</h2>
